13a07361c1740dfbc3153cd78d186f4620c112e1
zeah <aril.twitter@gmail.com>
2018-06-18 14:05:00 +0200

    55

--- EM-casinoliste.php
+++ EM-casinoliste.php
@@ -3,27 +3,9 @@
 	Plugin name: EM Casinolist
 	Description: List of casino offers
 	Version: 0.1.0.2
+	GitHub Plugin URI: zeah/EM-casinoliste
 */
 
-/*
-done:
-shortcode
-shortcode image
-shortcode signup link
-meta done with js
-added cpt to on-site search
-added served css when shortcode present
-taxonomy registered
-
-todo:
-taxonomy in shortcode
-customizer
-code comments
-adding meta fields
-css
-mobile css
-tablet css
-*/
 
 defined( 'ABSPATH' ) or die( 'Blank Space' );
 
